7e9f85990488bb951139d24b6ff6717396d22bda
manman4 <manchanr4@gmail.com>
2021-02-03 02:10:54 +0900

    a(n) = Sum_{i=0..n} binomial(n,i)^2*i!*4^i.

---
+++ 102/102773/102773_01.rb
@@ -0,0 +1,22 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A102773(n)
+  (0..n).inject(0){|s, i| s + ncr(n, i) ** 2 * 4 ** i * f(i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A102773(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
