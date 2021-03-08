36da06a0fc458e4a4af5e73cca403a915ef62432
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-08 23:24:07 +0900

    "Sloping binary numbers": write numbers in binary under each other (right-justified), read diagonals in upward direction, convert to decimal.

---
+++ 102/102370/102370_01.rb
@@ -0,0 +1,17 @@
+# m^n-n-1‚Ü‚Å
+def A(m, n)
+  ary = [0]
+  n.times{|i|
+    (m ** i - i..m ** (i + 1) - i - 2).each{|j|
+      ary << (0..i).inject(0){|s, k| s + (j + k).to_s(m)[-k - 1].to_i * m ** k}
+    }
+  }
+  ary
+end
+
+ary = A(2, 15)
+(0..10000).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
