1b9212fd12da1b84b5bb5d2b09842e1741ed43d9
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-08 23:07:46 +0900

    The sequence solves the following problem: find all the pairs (i,j) such that i divides 1+j+j^2 and j divides 1+i+i^2. In fact, the pairs (a(n),a(n+1)), n>0, are all the solutions.

---
+++ 101/101368/101368_01.rb
@@ -0,0 +1,26 @@
+# 0����n�܂�
+def A(m, n)
+  a = Array.new(m, 1)
+  ary = [1]
+  while ary.size < n + 1
+    i = a[1..-1].inject(0){|s, i| s + i * i} + a[1..-1].inject(:+) + 1
+    break if i % a[0] > 0
+    a = *a[1..-1], i / a[0]
+    ary << a[0]
+  end
+  ary
+end
+
+def A101368(n)
+  A(2, n - 1)
+end
+
+n = 1500
+ary = A101368(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
