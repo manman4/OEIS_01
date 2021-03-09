f362299be578685a849979327aadb5e922b33913
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-09 20:27:41 +0900

    Coefficients of the A-Bailey Mod 9 identity.

---
+++ 104/104467/104467_01.rb
@@ -0,0 +1,21 @@
+def A(k, m, n)
+  s = 0
+  (1..n).each{|i|
+    s += i if n % i == 0 && (i % k == m || i % k == k - m)
+  }
+  s
+end
+def A104467(n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| A(9, 4, i) - A(9, 3, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  ary
+end
+
+m = 10100
+ary = A104467(m)
+(0..10000).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
