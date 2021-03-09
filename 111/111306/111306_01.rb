e04607f8f74263134710f154448d17792b8c5b44
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-10 00:30:03 +0900

    d_12(n), tau_12(n), number of ordered factorizations of n as n = rstuvwxyzabc (12-factorizations).

---
+++ 111/111306/111306_01.rb
@@ -0,0 +1,24 @@
+# aryÇÃÉTÉCÉYÇÕn
+def A(ary, n)
+  a = [0] + ary
+  b = []
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += a[j] if i % j == 0
+    }
+    b << s
+  }
+  b
+end
+
+n = 10000
+ary = Array.new(n, 1)
+11.times{
+  ary = A(ary, n)
+}
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
