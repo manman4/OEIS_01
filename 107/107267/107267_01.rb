19dc87e981467e282d42c800ae6c9b1c60e42c87
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-09 21:06:54 +0900

    A square array of Motzkin related transforms, read by antidiagonals.

---
+++ 107/107267/107267_01.rb
@@ -0,0 +1,34 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..k).inject(0){|s, i| s + n ** i * ncr(k, i) * ncr(i + 1, k - i + 1) / (i + 1)}
+end
+
+def B(k, n)
+  (0..n).map{|i| A(k, i)}
+end
+
+def A107267(n)
+  a = []
+  (0..n).each{|i| a << B(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 139
+ary = A107267(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
