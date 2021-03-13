cdb5211fba0132ed3868b10ac43c23a533ca1021
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-13 23:48:27 +0900

    Numerator of zeta(4n)/zeta(2n)^2 (with a(0)=2 instead of -2).

---
+++ 114/114362/114362_01.rb
@@ -0,0 +1,34 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def A(n)
+  a = bernoulli(4 * n)
+  ary = []
+  (0..n).each{|i|
+    ary << -2 * a[4 * i] / (a[2 * i] ** 2 * ncr(4 * i, 2 * i))
+  }
+  ary
+end
+
+n = 200
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i].abs.numerator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
