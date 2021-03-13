f91b2d7232424ba7e7e57d03eb8a0dbd1d261121
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-14 00:18:08 +0900

    Third convolution of A115140.

---
+++ 115/115142/115142_01.rb
@@ -0,0 +1,40 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1] + (1..n).map{|i| -ncr(2 * (i - 1), i - 1) / i}
+  power(ary, k, n)
+end
+
+n = 1800
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
