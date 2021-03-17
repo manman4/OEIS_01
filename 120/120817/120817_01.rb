babb1e3654182cf7a7b437cb3286d872bb7b70d5
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-18 00:06:51 +0900

    10-adic integer x=...07839804103263499879186432 satisfying x^5 = x; also x^3 = -x = A120818; (x^2)^3 = x^2 = A091664; (x^4)^2 = x^4 = A018248.

---
+++ 120/120817/120817_01.rb
@@ -0,0 +1,23 @@
+def A(n)
+  s1, s2 = 2, 8
+  n.times{|i|
+    m = 10 ** (i + 1)
+    (0..9).each{|j|
+      k1, k2 = j * m + s1, (9 - j) * m + s2
+      if (k1 ** 5 - k1) % (m * 10) == 0 && (k2 ** 5 - k2) % (m * 10) == 0
+        s1, s2 = k1, k2
+        break
+      end
+    }
+  }
+  s1.to_s.reverse
+end
+
+n = 9999
+str = A(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  # nil.to_i‚Í0
+  puts str[i].to_i
+}
