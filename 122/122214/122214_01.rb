45a05e98a648c166b37a853a79b99add5f231bae
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-20 16:26:31 +0900

    Numerators in infinite products for Pi/2, e and e^gamma (reduced).

---
+++ 122/122214/122214_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (1..n).inject(1){|s, i| s * i ** ((-1) ** (i % 2) * ncr(n - 1, i - 1))}
+end
+
+n = 20
+(1..n).each{|i|
+  j = A(i).numerator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
