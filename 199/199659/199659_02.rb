31ae0d97be05951ede5c18a8328fbf295c195ee2
manman4 <manchanr4@gmail.com>
2021-03-27 20:04:41 +0900

    Expansion of q^(1/4) * (eta(q) / eta(q^3))^3 in powers of q.

--- 199/199659/199659_02.rb
+++ 199/199659/199659_02.rb
@@ -7,7 +7,7 @@ end
 def A(k, n)
   ary = [1]
   a = [0] + (1..n).map{|i| s(k, i)}
-  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s -= k * a[j] * ary[-j]} / i}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - k * a[j] * ary[-j]} / i}
   ary
 end
 
