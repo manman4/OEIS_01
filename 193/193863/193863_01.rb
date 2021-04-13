ba17f1e759bd6da5ab4bf527ef411668bf40dfd1
manman4 <manchanr4@gmail.com>
2021-04-14 01:53:42 +0900

    Expansion of Product_{n>=0} (1 + q*(-q^2)^n) / (1 - q*(-q^2)^n).

---
+++ 193/193863/193863_01.rb
@@ -0,0 +1,51 @@
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def A(n)
+  ary = [1]
+  (0..n / 2).each{|i|
+    b_ary = Array.new(2 * i + 2, 0)
+    b_ary[0], b_ary[-1] = 1, (-1) ** (i % 2)
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def B(n)
+  ary = [1]
+  (0..n / 2).each{|i|
+    b_ary = Array.new(2 * i + 2, 0)
+    b_ary[0], b_ary[-1] = 1, (-1) ** (i % 2 + 1)
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+n = 1100
+ary = mul(A(n), I(B(n), n), n)
+(0..1000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
