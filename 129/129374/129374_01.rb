7606c520bb706e15e6e85489622c68edf0d8ad23
manman4 <manchanr4@gmail.com>
2021-04-14 00:03:05 +0900

    G.f. satisfies: A(x) = 1/(1-x) * A(x^2)*A(x^3)*A(x^4)*...*A(x^n)*...

---
+++ 129/129374/129374_01.rb
@@ -0,0 +1,40 @@
+def S(n)
+  a = [0, 1]
+  (2..n).each{|i|
+    s = 1
+    (1..i - 1).each{|j|
+      s += a[j] if i % j == 0
+    }
+    a << s
+  }
+  [0, 1] + (2..n).map{|i| a[i] / 2}
+end
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+def B(n)
+  ary1 = S(n)
+  ary2 = Array.new(n + 1, 1)
+  A(ary1, ary2, n)
+end
+
+n = 10000
+ary = B(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
