3855b78314caea8c73ab5cce6604806f80ceff82
manman4 <manchanr4@gmail.com>
2021-04-13 23:59:17 +0900

    Third sequence in solution to congruent number 5 problem.

---
+++ 129/129208/129208_01.rb
@@ -0,0 +1,23 @@
+def A(u, v, w, x, n)
+  a, b, c, d = u, v, w, x
+  ary = [u]
+  cnt = 0
+  while cnt < n
+    i = -144 * b * d + 2257 * c * c
+    break if i % a > 0
+    a, b, c, d = b, c, d, i / a
+    ary << a
+    cnt += 1
+  end
+  ary
+end
+
+n = 100
+ary = A(1, 2, 41, 1562, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
