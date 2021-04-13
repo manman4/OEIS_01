59a21bf70e2d030106f414d04db1f011f07950e8
manman4 <manchanr4@gmail.com>
2021-04-13 23:28:20 +0900

    Ramanujan numbers (A000594) read mod 25.

---
+++ 126/126833/126833_01.rb
@@ -0,0 +1,42 @@
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m次以下を取り出す
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A000594(n)
+  ary = Array.new(n + 1, 0)
+  # ヤコビの公式の必要なところだけ取り出す
+  i = 0
+  j, k = 2 * i + 1, i * (i + 1) / 2
+  while k <= n
+    i & 1 == 1? ary[k] = -j : ary[k] = j
+    i += 1
+    j, k = 2 * i + 1, i * (i + 1) / 2
+  end
+  # 8乗してx倍
+  power(ary, 8, n).unshift(0)[1..n]
+end
+
+n = 10100
+ary = A000594(n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1] % 25
+}
