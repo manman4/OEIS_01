e9159174b3757e46de7882cb3d99d406ceef8d2c
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-22 20:16:15 +0900

    A156348 * A000010

---
+++ 156/156834/156834_01.rb
@@ -0,0 +1,25 @@
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    s += phi(i) * ncr(i + n / i - 2, i - 1) if n % i == 0
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
