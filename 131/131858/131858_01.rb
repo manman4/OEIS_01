51648e6faa736814180b6a7afbd6f984e0db76a7
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-26 21:34:40 +0900

    Numbers m such that z(m)=(1,0) with z as defined in A131851.

---
+++ 131/131858/131858_01.rb
@@ -0,0 +1,23 @@
+def A(n)
+  s = 0
+  n.to_s(2).split('').reverse.each_with_index{|e, i| s += e.to_i * 1i ** i}
+  s
+end
+
+def B(n)
+  s = -1
+  ary = []
+  while ary.size < n
+    s += 1
+    ary << s if A(s) == 1
+  end
+  ary
+end
+
+n = 10000
+ary = B(n)
+(1..n).each{|i| 
+  print i
+  print ' '
+  puts ary[i - 1]
+}
