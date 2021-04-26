f59e923d61b54067055887ea71229b17842935c8
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-26 21:37:06 +0900

    Number of partitions of n where odd parts are distinct or repeated once.

---
+++ 131/131945/131945_01.rb
@@ -0,0 +1,36 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def A131945(n)
+  ary5 = [1]
+  3.step(n, 6){|i|
+    b_ary = Array.new(i + 1, 0)
+    b_ary[0], b_ary[-1] = 1, -1
+    ary5 = mul(ary5, b_ary, n)
+  }
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  (1..n).each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  mul(ary5, ps, n)
+end
+
+n = 1000
+ary = A131945(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
