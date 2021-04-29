427f8cf1ec2a94058d831cb54d5da5019f70f1cf
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-29 15:25:04 +0900

    a(n) = 20*a(n-1) - 64*a(n-2) for n > 1; a(0) = 1, a(1) = 20.

---
+++ 166/166984/166984_01.rb
@@ -0,0 +1,19 @@
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+n = 1000
+ary = I([1, -20, 64] + [0] * (n - 1), n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
