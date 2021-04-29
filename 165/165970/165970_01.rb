b14d32e25e5a0cc4bd60385a6fadeb578e8736ac
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-29 14:57:03 +0900

    a(n) = sqrt( superfactorial(4n) / factorial(2n) ).

---
+++ 165/165970/165970_01.rb
@@ -0,0 +1,25 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+
+def A308674(n)
+  s = 1
+  ary = [1]
+  (1..n).each{|i|
+    s *= 2 * f(4 * i - 3) * f(4 * i - 1)
+    ary << s
+  }
+  ary
+end
+
+n = 20
+ary = A308674(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
