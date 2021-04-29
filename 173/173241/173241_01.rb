01d1df6649b3f2dd3eaa30c2ceaf28aed0e18e35
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-29 21:26:17 +0900

    Euler transform of A051064, the ruler function sequence for k=3.

---
+++ 173/173241/173241_01.rb
@@ -0,0 +1,34 @@
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def A173241(n)
+  I(A((0..Math.log(n, 3)).map{|i| [3 ** i, 1]}, n), n)
+end
+
+# n = 46
+m = 11000
+n = 10000
+ary = A173241(m)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
