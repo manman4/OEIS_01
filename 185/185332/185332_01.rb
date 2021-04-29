b62ba2fddd313adb80d8d437f57d9804e60c48d3
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-29 23:44:56 +0900

    Numerators of u(n) where u(n) = (u(n-1) + u(n-2)) / u(n-3), with u(1) = u(2) = u(3) = 1.

---
+++ 185/185332/185332_01.rb
@@ -0,0 +1,20 @@
+def u(n)
+  a = Array.new(3, 1r)
+  ary = [1]
+  while ary.size < n
+    j = a[1] + a[-1]
+    a = *a[1..-1], j / a[0]
+    ary << a[0]
+  end
+  ary
+end
+
+n = 300
+ary = u(n)
+(1..n).each{|i|
+  j = ary[i - 1].numerator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
