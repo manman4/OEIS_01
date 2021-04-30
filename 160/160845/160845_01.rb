aca6e773db173a70c911a247a0d1508f62e3cd05
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 23:51:49 +0900

    Number of lines through at least 2 points of a 5 X n grid of points.

---
+++ 160/160845/160845_01.rb
@@ -0,0 +1,24 @@
+def f(m, n, k)
+  s = 0
+  t, u = n / k, m / k
+  (-t).upto(t){|x|
+    (-u).upto(u){|y|
+      if x.gcd(y) == 1
+        s += (n - (k * x).abs) * (m - (k * y).abs)
+      end
+    }
+  }
+  s
+end
+
+def g(m, n)
+  return 0 if m == 0 || n == 0
+  (f(m, n, 1) - f(m, n, 2)) / 2
+end
+
+n = 1000
+(0..n).each{|i|
+  print i
+  print ' '
+  puts g(5, i)
+}
