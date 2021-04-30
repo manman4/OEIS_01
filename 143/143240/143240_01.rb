7f74d5f55b55d4b920e794df9884b1db85ba0dce
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 20:34:45 +0900

    Expansion of Product_{k>0} (1 - x^(3*k)) / (1 - x^(3*k - 1)).

---
+++ 143/143240/143240_01.rb
@@ -0,0 +1,20 @@
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == m}
+  s
+end
+
+def A143240(n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| s(3, 2, i) - s(3, 0, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + ary[-j] * s_ary[j]} / i}
+  ary
+end
+
+n = 10000
+ary = A143240(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
