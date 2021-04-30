6d841d4c8505eb05d4d4849b08ebe3793511da55
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 21:34:10 +0900

    McKay-Thompson series of class 12c for the Monster group with a(0) = -4.

---
+++ 186/186930/186930_01.rb
@@ -0,0 +1,20 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| 4 * s(1, i) + 9 * s(6, i) - 3 * s(2, i) - 4 * s(3, i) - s(4, i) - 5 * s(12, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10000
+ary = A(n + 1)
+-1.upto(n){|i|
+  print i
+  print ' '
+  puts ary[i + 1]
+}
