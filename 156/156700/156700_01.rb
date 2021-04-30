a29c01823f9c3724e9fa1dc1584141c7d441067a
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 15:52:15 +0900

    Number of partitions of the set of odd numbers {1, 3, 5, ..., 4*n-1} into two subsets with equal sum.

---
+++ 156/156700/156700_01.rb
@@ -0,0 +1,25 @@
+def A(n)
+  ary = [1]
+  s_ary = [0]
+  (1..n).each{|i|
+    s_ary << s_ary[-1] + 2 * i - 1
+  }
+  m = s_ary[-1]
+  a = Array.new(m + 1){0}
+  a[0] = 1
+  (1..n).each{|i|
+    b = a.clone
+    (0..[s_ary[i - 1], m - (2 * i - 1)].min).each{|j| b[j + 2 * i - 1] += a[j]}
+    a = b
+    s_ary[i] % 2 == 0 ? ary << a[s_ary[i] / 2] : ary << 0
+  }
+  ary
+end
+
+n = 1000
+ary = A(2 * n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[2 * i] / 2
+}
