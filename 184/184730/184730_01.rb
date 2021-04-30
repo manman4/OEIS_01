294aea1e13cc0492d3dc44f62e537f76ad03d04f
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 16:02:54 +0900

    G.f.: exp( Sum_{n>=1} A184731(n)*x^n/n ) where A184731(n) = Sum_{k=0..n} C(n,k)^(k+1).

---
+++ 184/184730/184730_01.rb
@@ -0,0 +1,25 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (0..n).inject(0){|s, i| s + ncr(n, i) ** (i + 1)}
+end
+
+def B(n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| A(i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 100
+ary = B(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
