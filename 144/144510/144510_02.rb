9df0d66040d00a1c820fa81232949bb023a8c242
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 21:52:59 +0900

    Array T(n,k) (n >= 1, k >= 0) read by downwards antidiagonals: T(n,k) = total number of partitions of [1, 2, ..., i] into exactly k nonempty blocks, each of size at most n, for any i in the range n <= i <= k*n.

---
+++ 144/144510/144510_02.rb
@@ -0,0 +1,51 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  ary = [0] + (1..n).map{|i| 1r / f(i)}
+  ary = power(ary, k, k * n)
+  ((k..k * n).inject(0){|s, i| s + f(i) * ary[i]} / f(k)).to_i
+end
+
+def B(n)
+  ary = []
+  (1..n).each{|i|
+    (1..i).each{|j|
+      ary << A(i - j, j)
+    }
+  }
+  ary
+end
+
+n = 50
+ary = B(n)
+(1..ary.size).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
