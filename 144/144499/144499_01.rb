3f8e896cd5575254f3dfb572e29833fea35e00b1
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 21:57:47 +0900

    Column 3 of array in A144502.

---
+++ 144/144499/144499_01.rb
@@ -0,0 +1,14 @@
+def A(k, n)
+  ary = Array.new(n + k + 1, 1) + [0]
+  k.times{|i| (0..n + k - i - 1).each{|j| ary[j] = j * ary[j - 1] + ary[j + 1]}}
+  ary[0..n]
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i, 3)[-1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
