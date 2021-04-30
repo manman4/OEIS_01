c57443cc190c4053c138f140be0c9b4d501099ad
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 19:19:05 +0900

    Least number m such that binomial(n+m, m) mod m = 1.

---
+++ 133/133906/133906_01.rb
@@ -0,0 +1,21 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  k = 2
+  while ncr(n + k, k) % k != 1
+    k += 1
+  end
+  k
+end
+
+n = 1000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
