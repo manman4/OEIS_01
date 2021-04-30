d00b76f8d120061d25438036f4493352be92b095
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-30 20:45:06 +0900

    Expansion of eta(q^3) * eta(q^4) * eta(q^5) * eta(q^60) / (eta(q) * eta(q^12) * eta(q^15) * eta(q^20)) in powers of q.

---
+++ 143/143752/143752_01.rb
@@ -0,0 +1,20 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(2, i) + s(6, i) + s(10, i) + s(30, i) - s(1, i) - s(12, i) - s(15, i) - s(20, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10000
+ary = A(n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
