014c07289fb51974af1f74a87d50b93dab9ef6cf
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-08 01:07:17 +0900

    Numbers k such that (j^k + k^j) == 0 (mod k+j), j=5 case.

---
+++ 114/114980/114980_01.rb
@@ -0,0 +1,26 @@
+def pow(a, m, mod)
+  return 1 % mod if m == 0
+  k = pow(a, m >> 1, mod)
+  k *= k
+  return k % mod if m & 1 == 0
+  return k * a % mod
+end
+
+def A(k, n)
+  ary = []
+  i = 1
+  while ary.size < n
+    mod = i + k
+    ary << i if (pow(k, i, mod) + pow(i, k, mod)) % mod == 0
+    i += 1
+  end
+  ary
+end
+
+n = 10000
+ary = A(5, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
