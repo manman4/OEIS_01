bc4de0ea51507cefeb0bc9549d7533b510c2dbee
manman4 <7023566+manman4@users.noreply.github.com>
2021-09-23 12:36:17 +0900

    a(1) = 1, a(2) = 7, a(n+2) = 7*a(n+1)+(n+1)*(n+3)*a(n).

---
+++ 142/142990/142990_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  ary = [0, 1]
+  a, b = 1, k
+  (2..n).each{|i|
+    a, b = b, k * b + i * (i + 2) * a
+    ary << a
+  }
+  ary
+end
+
+n = 1000
+ary = A(7, n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
