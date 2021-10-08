76c5cf25996004ba570cd60548759756c061f048
manman4 <7023566+manman4@users.noreply.github.com>
2021-10-08 22:22:55 +0900

    a(n) = Sum_{d|n} C(n,d)^(n/d).

---
+++ 174/174464/174464_01.rb
@@ -0,0 +1,21 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    s += ncr(n, i) ** (n / i) if n % i == 0
+  }
+  s
+end
+
+n = 1000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
