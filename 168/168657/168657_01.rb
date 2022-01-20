5bca83cd91334e8745bce3156920b73bba4ac0c2
manman4 <7023566+manman4@users.noreply.github.com>
2022-01-20 22:38:09 +0900

    Number of partitions of n such that the number of parts is divisible by the smallest part.

---
+++ 168/168657/168657_01.rb
@@ -0,0 +1,14 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+
+def f(n)
+  cnt = 0
+  partition(n, 1, n).each{|ary|
+    cnt += 1 if  ary.size % ary[-1] == 0
+  }
+  cnt
+end
+
+p (1..20).map{|i| f(i)}
