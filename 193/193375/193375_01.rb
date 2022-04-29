2fbf75ff66e4c996b09a736910a3e37486c188f6
manman4 <7023566+manman4@users.noreply.github.com>
2022-04-29 22:59:34 +0900

    E.g.f.: A(x) = exp( Sum_{n>=1} x^(n*(n+1)/2) ).

---
+++ 193/193375/193375_01.rb
@@ -0,0 +1,29 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  ary = [1]
+  (1..n).each{|i|
+    m = f(i - 1)
+    s = 0
+    (1..i).each{|j|
+      k = j * (j + 1) / 2
+      break if k > i
+      s += k * ary[i - k] * m / f(i - k)
+    }
+    ary << s
+  }
+  ary
+end
+
+n = 500
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
