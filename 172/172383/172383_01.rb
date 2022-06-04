20d4592e3165137e186a82eb63db3dc64a16d830
manman4 <7023566+manman4@users.noreply.github.com>
2022-06-04 22:24:32 +0900

    a(0)=1, otherwise a(n) = Sum_{k=0..floor((n-1)/2)} binomial(n-k-1,k)*a(n-1-2*k).

---
+++ 172/172383/172383_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  ary = [1]
+  (1..n).each{|i| 
+    ary << (0..(i - 1) / 2).inject(0){|s, j| s + ncr(i - j - 1, j) * ary[-1 - 2 * j]}
+  }
+  ary
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
