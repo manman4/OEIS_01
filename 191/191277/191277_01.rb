ef07fd685df2f85ec7af68d948b62d5bb10c9ac6
manman4 <7023566+manman4@users.noreply.github.com>
2022-06-30 15:42:14 +0900

    Expansion of e.g.f. 1/(1 - sinh(x)*cosh(x)).

---
+++ 191/191277/191277_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (0..(i - 1) / 2).inject(0){|s, j| s + 4 ** j *  ncr(i, 2 * j + 1) * ary[-2 * j - 1]}
+  }
+  ary
+end
+
+n = 500
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
