1e669cfe7d2d7fc6bec16cb56d74d796c8049fe0
manman4 <7023566+manman4@users.noreply.github.com>
2022-10-15 20:03:52 +0900

    Let A(0)=1, B(0)=0 and C(0)=0. Let A(n+1) = - sum {k = 0..n) binomial(n,k)*C(k), B(n+1) = sum {k = 0..n) binomial(n,k)*A(k) and C(n+1) = sum {k = 0..n) binomial(n,k)*B(k). This entry gives the sequence B(n).

---
+++ 143/143631/143631_01.rb
@@ -0,0 +1,25 @@
+def A(n)
+  a = [1]
+  ary = [0]
+  (1..n).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (j + 1) * a[j + 1]
+    }
+    a = b
+    # i > 0 のときa[0] = 0
+    ary << (0..(i - 1) / 3).inject(0){|s, j| s + (-1) ** j * a[3 * j + 1]}
+  }
+  ary
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
