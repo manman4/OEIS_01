26c567af89de3dff000878a588868b268572d3a3
manman4 <7023566+manman4@users.noreply.github.com>
2023-01-24 14:10:58 +0900

    Expansion of (1/(1-x)^3)*c(x/(1-x)^3), c(x) the g.f. of A000108.

---
+++ 162/162481/162481_01.rb
@@ -0,0 +1,23 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| 
+    ary << (0..i - 1).inject(ncr(i + k - 1, k - 1)){|s, j| s + ary[j] * ary[-1 - j]}
+  }
+  ary
+end
+
+n = 1100
+m = 1000
+ary = A(3, n)
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
