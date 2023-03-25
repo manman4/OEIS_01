bb6429f3a088ab543f8f7226994abd1a270a0b03
manman4 <7023566+manman4@users.noreply.github.com>
2023-03-25 20:16:46 +0900

    G.f. A(x) satisfies: A(x)^3 = A(x^3) + 3*x.

---
+++ 107/107092/107092_01.rb
@@ -0,0 +1,42 @@
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m次以下を取り出す
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  ary = [1, 1]
+  (2..n).each{|i|
+    if i % k > 0
+      ary << -power(ary, k, i)[-1] / k
+    else
+      ary << (ary[i / k] - power(ary, k, i)[-1]) / k
+    end
+  }
+  ary
+end
+
+n = 1000
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
