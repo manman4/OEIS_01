9a37772173e8b6736a95c917a8351f073616d549
manman4 <7023566+manman4@users.noreply.github.com>
2023-04-22 18:10:58 +0900

    E.g.f. satisfies A(x) = exp(x + x^2*A(x)^2).

---
+++ 143/143768/143768_01.rb
@@ -0,0 +1,18 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  m = f(n)
+  (0..n / 2).inject(0){|s, i| s + m * (k / 2r) ** i * (2 * i + 1) ** (n - i - 1) / (f(i) * f(n - 2 * i))}.to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(2, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
