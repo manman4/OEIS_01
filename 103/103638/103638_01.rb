e64e1b4ebe23ba02826b649ec3c97f391dcb8c50
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-01 00:53:58 +0900

    a(n) = Sum_{d|n, d==2 mod 3} d^2.

---
+++ 103/103638/103638_01.rb
@@ -0,0 +1,16 @@
+def A(n, mod, k)
+  s = 0
+  (1..n).each{|i|
+    if n % i == 0
+      s += i * i if i % mod == k
+    end
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(i, 3, 2)
+}
