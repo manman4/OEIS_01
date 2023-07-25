9631a2f98733264c556ffc76f9595588e5bf59d7
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-25 20:22:04 +0900

    G.f. satisfies A(x) = 1 + x*A(2x)^8.

---
+++ 171/171210/171210_01.rb
@@ -0,0 +1,40 @@
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m次以下を取り出す
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+# G.f. satisfies: A(x) = 1 + x*A(k*x)^m.
+def A(k, m, n)
+  ary = [1]
+  (1..n).each{|i|
+    a = (0..i - 1).map{|i| k ** i * ary[i]}
+    ary[i] = power(a, m, i - 1)[-1]
+  }
+  ary
+end
+
+n = 100
+ary = A(2, 8, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
