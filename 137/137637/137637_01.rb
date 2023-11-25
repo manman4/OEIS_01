f61617d817d81ecf5b82cb57bc0356d04d3c3590
manman4 <7023566+manman4@users.noreply.github.com>
2023-11-25 22:33:16 +0900

    a(n) = Sum_{k=0..n} C(2k+2,k)*C(2k+2,n-k) ; equals row 2 of square array A137634 ; also equals the convolution of A137635 and the self-convolution of A073157.

---
+++ 137/137637/137637_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  return 1 if n == 0
+  (0..n).inject(0){|s, i| s + ncr(2 * i + 2, i) * ncr(2 * i + 2, n - i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
