ebf92b8e928340ff2bbce5cd0651279215e2acfa
manman4 <7023566+manman4@users.noreply.github.com>
2024-02-24 00:02:39 +0900

    Number of permutations of [n] having no adjacent 4-cycles, i.e., no cycles of the form (i, i+1, i+2, i+3).

---
+++ 177/177253/177253_01.rb
@@ -0,0 +1,17 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n, k)
+  (0..n / k).inject(0){|s, i| s + (-1) ** i * f(n - (k - 1) * i) / f(i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i, 4)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
