1e967badea6a268e646597d78d41c44c409b1189
manman4 <7023566+manman4@users.noreply.github.com>
2025-07-06 23:34:53 +0900

    G.f. A(x) satisfies A(x) = 1/(1 - x*A(2*x)^4).

--- 171/171194/171194_02.rb
+++ 171/171194/171194_02.rb
@@ -1,9 +1,9 @@
-# a(0) = 1; a(n) = 2^(n-1) * Sum_{i,j,k>=0 and i+j+k=n-1} 1/2^i * a(i) * a(j) * a(k)
+# a(0) = 1; a(n) = 2^(n-1) * Sum_{i, j, k, l, m>=0 and i+j+k+l+m=n-1} (1/2)^i * a(i) * a(j) * a(k) * a(l) * a(m).
 def a(n, k)
   ary = [1]
   (1..n).each{|m|
     s = 0
-    # i,j,k>=0 and i+j+k=m-1を列挙
+    # i,j,k,l,m>=0 and i+j+k+l+m=m-1を列挙
     (0..m - 1).to_a.repeated_permutation(k).each{|i|
       s += 1r / (2 ** i[0]) * (1..k).inject(1){|prod, j| prod * ary[i[j - 1]]} if i.sum == m - 1
     }
@@ -12,8 +12,8 @@ def a(n, k)
   ary
 end
 
-n = 50
-ary = a(n, 5)
+n = 30
+p ary = a(n, 5)
 (0..n).each{|i|
   j = ary[i]
   break if j.to_s.size > 1000
