e5fed0f652bb02f7382d9ffe55bc7eadb7f1b835
manman4 <7023566+manman4@users.noreply.github.com>
2026-03-04 23:37:59 +0900

    G.f. A(x) satisfies 4*A(x) = 3 + x + A(x)^3, starting with [1,1,3].

---
+++ 120/120590/120590_01.rb
@@ -0,0 +1,21 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(0) = 1; a(n) = (1/n) * Sum_{k=0..floor((n-1)/2)} 3^(n-1-2*k) * binomial(2*n-2-k,n-1) * binomial(n-1-k,k).
+def a(n)
+  ary = [1]
+  (1..n).each{|i| ary << (0..(i - 1) / 2).inject(0){|s, k| s + 3 ** (i - 1 - 2 * k) * ncr(2 * i - 2 - k, i - 1) * ncr(i - 1 - k, k)} / i}
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
