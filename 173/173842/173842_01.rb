46e3d7198e98932e53353c9d111c6e0500a01cbd
manman4 <7023566+manman4@users.noreply.github.com>
2026-08-05 18:22:03 +0900

    Number of permutations of 1..n with no adjacent pair summing to n+2.

---
+++ 173/173842/173842_01.rb
@@ -0,0 +1,24 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n,k) = Sum_{j=0..m} (-2)^j*binomial(m,j)*(n-j)! where m = max(0, floor((n-k+1)/2)).
+def a(n, k)
+  m = [0, (n - k + 1) / 2].max
+  (0..m).inject(0){|s, j| s + (-2)**j * ncr(m, j) * f(n - j)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = a(i, 2) 
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
